3e432de95b05e15e7f598d9448507ca5e59524af
tivvvv <18612553935@163.com>
2025-06-22 10:11:08 +0800

    feat: 制作底部栏样式.

---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header>Header</a-layout-header>
+      <a-layout-content>Content</a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://www.tivvvv.cn" target="_blank"> image-hub by tivvvv </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
