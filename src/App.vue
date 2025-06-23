d4a91ea89cc178c06468944d76238a374aa7ab71
tivvvv <18612553935@163.com>
2025-06-23 23:44:01 +0800

    feat: 引入umijs openapi工具.

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,11 @@
 
 <script setup lang="ts">
 import BasicLayout from '@/layouts/BasicLayout.vue'
+import { heartBeatUsingGet } from '@/api/testController.ts'
+
+heartBeatUsingGet().then((res) => {
+  console.log(res)
+})
 </script>
 
 <style scoped></style>
---
+++ src/request.ts
@@ -0,0 +1,41 @@
+import axios, { type AxiosInstance } from 'axios'
+import { message } from 'ant-design-vue'
+
+// 创建axios实例
+const myAxios: AxiosInstance = axios.create({
+  baseURL: 'http://localhost:8111',
+  timeout: 10000,
+  withCredentials: true,
+})
+
+// 请求拦截器
+axios.interceptors.request.use(
+  function (config) {
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  },
+)
+
+// 响应拦截器
+axios.interceptors.response.use(
+  function (response) {
+    const { data } = response
+    if (data.code === 40100) {
+      if (
+        !response.request.responseURL.include('/user/get/login') &&
+        !window.location.pathname.includes('/user/login')
+      ) {
+        message.warning(data.message)
+        window.location.href = `/user/login?redirect=${window.location.href}`
+      }
+    }
+    return response
+  },
+  function (error) {
+    return Promise.reject(error)
+  },
+)
+
+export default myAxios
